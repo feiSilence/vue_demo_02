6d8bb5232c364adb571f3c6a3b260d8d0f9ec113
feiSilence <Silence_fei@163.com>
2017-11-23 21:29:19 +0800

    commit_02

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# vue_demo_02
+﻿# vue_demo_02
 vue实战项目
+
+commit_01 在根组件写入(头部导航)公共组件，用router编写单页面组件，newGoodsData.js模拟商品数据，在shop.vue单页面引入并渲染数据，将单个商品做成组件shop-item.vue并传入页面；
+子级组件利用props接收父级传递的数据，给组件内元素添加事件处理函数；
+
+commit_02 <router-link :to="{name: 'Item', query: {itemId:item.sku_info[itemIndex].sku_id}}"></router-link>，通过router-link跳转到商品详情页，通过query传递商品id，详情页组件item.vue通过data () {return {itemId: this.$route.query.itemId}}接收id数据，computed: {}过滤详情页数据itemsData并渲染数据{{itemsInfo.title}}
+详情页中的交互，缩略图切换和颜色切换，@click="tableData(index)，获取循环数据中的index，通过id实现颜色切换，watch: {}监测数据变化
+
--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,33 @@ import '@/assets/css/goodsList.css'
 
 //引入单页面
 import Shop from '@/views/shop'
+import Item from '@/views/item'
+import Cart from '@/views/cart'
 
 export default new Router({
+  mode: 'history',
+  scrollBehavior (to, from, savePosition) {
+    if (savePosition) {
+      return savePosition
+    } else {
+      return {x: 0, y: 0}
+    }
+  },
   routes: [
     {
       path: '/',
+      name: 'Shop',
       component: Shop
+    },
+    {
+      path: '/item',
+      name: 'Item',
+      component: Item
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
     }
   ]
 })
